dc2857ae957415f56cac6446a18b128e67321f6d
Wreckitshoot <90283922+Wreckitshoot@users.noreply.github.com>
2024-02-27 12:03:11 -0500

    Tutorial 1

---
+++ GridGame/Assets/Scripts/GridManager.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public int numRows = 5;
+
+    public int numCols = 6;
+
+    public float padding = 0.1f;
+
+    [SerializeField] private GridTile tilePrefab;
+    [SerializeField] private TextMeshProUGUI text;
+    private void Awake()
+    {
+        InitGrid();
+
+    }
+    public void InitGrid()
+    {
+        for (int y = 0; y < numRows; y++)
+        {
+            for(int x = 0; x < numCols; x++)
+            {
+                GridTile tile = Instantiate(tilePrefab, transform);
+                Vector2 tilePos = new Vector2(x + padding*x, y+padding*y);
+                tile.transform.localPosition = tilePos;
+                tile.name = $"Tile_{x}_{y}";
+                tile.GridManager = this;
+                tile.gridCoords = new Vector2Int(x, y);
+            }
+        }
+    }
+    public void OnTileHoverEnter(GridTile gridTile)
+    {
+        text.text = gridTile.gridCoords.ToString();
+    }
+    public void OnTileHoverExit(GridTile gridTile)
+    {
+        text.text = "";
+    }
+}
---
+++ GridGame/Assets/Scripts/GridTile.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTile : MonoBehaviour
+{
+    public GridManager GridManager;
+    public Vector2Int gridCoords;
+    private SpriteRenderer _spriteRenderer;
+    private Color _defaultColour;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _defaultColour = _spriteRenderer.color;
+    }
+    private void OnMouseOver()
+    {
+        GridManager.OnTileHoverEnter(this);
+        setColour(Color.black);
+    }
+    private void OnMouseExit()
+    {
+        GridManager.OnTileHoverExit(this);
+        resetColour();
+    }
+    public void setColour(Color colour)
+    {
+        _spriteRenderer.color = colour;
+    }
+    public void resetColour()
+    {
+        _spriteRenderer.color = _defaultColour;
+    }
+}
